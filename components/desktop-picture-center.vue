6bfd95de0f21d4356e6f2fc4601f9c09ab00e5f2
Elodin <jcassola96@gmail.com>
2022-07-12 14:29:43 -0400

    make picture center component

---
+++ components/desktop-picture-center.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="picture-center">
+    <img src="~/assets/pictures/bg/fire.svg" width="500" height="600">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.picture-center{
+  background: url("assets/pictures/bg/center-bg.png");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 75%;
+  border-left: 7px solid #2F2727;
+  border-right: 7px solid #2F2727;
+}
+</style>
--- pages/pictures.vue
+++ pages/pictures.vue
@@ -1,14 +1,22 @@
 <template>
-  <h1>Pictures</h1>
+  <div class="picture">
+    <desktop-picture-center />
+  </div>
 </template>
 
 <script>
+import desktopPictureCenter from '~/components/desktop-picture-center.vue'
 export default {
   name: 'PicturesPage',
+  components: { desktopPictureCenter },
   layout: ({ isMobile }) => isMobile ? 'mobile' : 'default'
 }
 </script>
 
 <style>
-
+.picture{
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
 </style>
