f51b89caeb244d1ba7338a68fdeb17cf9f64ea29
Elodin <jcassola96@gmail.com>
2022-07-14 09:13:26 -0400

    add locales files

---
+++ locales/en.js
@@ -0,0 +1,9 @@
+export default {
+  contacts: 'Contacts',
+  videos: 'Videos',
+  photos: 'Photos',
+  partners: 'Partners',
+  credits: 'Credits',
+  certificate: 'Certificate',
+  equipment: 'Equipment List',
+}
