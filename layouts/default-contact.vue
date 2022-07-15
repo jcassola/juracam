2af7774741d09614c8d5e4c0a5ca11915d274d31
Elodin <jcassola96@gmail.com>
2022-07-15 16:00:37 -0400

    display flex default contact layout

--- layouts/default-contact.vue
+++ layouts/default-contact.vue
@@ -1,37 +1,47 @@
 <template>
-  <div>
-    <language-selector />
+  <div class="parent">
+    <div class="top" />
+    <!-- <language-selector /> -->
     <div class="main">
       <div class="container">
         <nuxt class="pages" />
         <NavBar class="my-navbar" />
       </div>
     </div>
+    <div class="footer" />
   </div>
 </template>
 
 <script>
-import LanguageSelector from '~/components/language-selector.vue'
+// import LanguageSelector from '~/components/language-selector.vue'
 import NavBar from '~/components/navbar.vue'
 
 export default {
   components: {
     NavBar,
-    LanguageSelector
+    // LanguageSelector
   }
 }
 </script>
 
 <style scoped>
-  .main{
-    background: url("assets/pictures/bg/tapes.svg");
+  .parent{
+    display: flex;
+    flex-direction: column;
     height: 100vh;
     width: 100vw;
-    background-position: center;
-    background-repeat: repeat;
-    background-size: contain;
+  }
+  .top, .footer {
+    height: 14%;
+    width: 100%;
+    background-color: black;
+  }
+  .main{
+    height: 72%;
+    width: 100%;
 
     display: flex;
+    flex-direction: row;
     align-items: center;
   }
   .pages{
@@ -42,10 +52,8 @@ export default {
   .container{
     display: flex;
     flex-direction: row;
-    height: 76%;
+    height: 100%;
     width: 100%;
-    margin-top: 10px;
-
   }
   .my-navbar{
     width: 33.33%;
