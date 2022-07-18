3b5bf904d23d359eec6964f8c67ebca0c2483ec7
Elodin <jcassola96@gmail.com>
2022-07-18 11:54:31 -0400

    add new bg to vertical asides

--- layouts/mobile.vue
+++ layouts/mobile.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="parent">
     <div class="left" />
-    <language-selector />
+    <!-- <language-selector /> -->
     <div class="main">
       <div class="container">
         <mobile-top />
@@ -14,14 +14,14 @@
 </template>
 
 <script>
-import LanguageSelector from '~/components/language-selector.vue'
+// import LanguageSelector from '~/components/language-selector.vue'
 import MobileTop from '~/components/mobile-top.vue'
 import NavBar from '~/components/navbar.vue'
 
 export default {
   components: {
     NavBar,
-    LanguageSelector,
+    // LanguageSelector,
     MobileTop
   }
 }
@@ -37,10 +37,10 @@ export default {
   .left, .right{
     width: 10%;
 
-    background: url("assets/pictures/bg/tapes-vertical.png");
+    background: url("assets/pictures/bg/tapes-vertical.png"), url("assets/pictures/bg/bg-vertical.png");
     background-position: center;
     background-repeat: repeat;
-    background-size: cover;
+    background-size: cover, contain;
   }
   .main{
     width: 80%;
